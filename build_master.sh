3685077032911dd98bde4d5955b714edf483ab27
Aruato <aruato@qq.com>
2022-04-17 15:36:08 +0800

    build: Import my script
    Signed-off-by: Aruato <aruato@qq.com>

--- build_master.sh
+++ build_master.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+VERSION="$(cat version)"
 
-if [[ "${1}" != "skip" ]] ; then
-	./build_clean.sh
-	./build_kernel.sh stock "$@" || exit 1
-fi
+echo "Compiling kernel"
+START=$(date +"%s")
 
-VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+sed -i -e 's@"want_initramfs"@"skip_initramfs"@g' init/initramfs.c
+cp defconfig .config
+make -j$(nproc --all) || exit 1
 
-if [ -e boot.img ] ; then
-	rm arter97-kernel-$VERSION.zip 2>/dev/null
-	cp boot.img arter97-kernel-$VERSION.img
-
-	# Pack AnyKernel3
-	rm -rf kernelzip
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
+	rm Moob-kernel-*.zip 2>/dev/null
+	rm -rf kernelzip 2>/dev/null
 	mkdir kernelzip
 	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
+kernel.string=moob-kernel-$(cat version)-@xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -32,13 +32,17 @@ block=/dev/block/bootdevice/by-name/boot
 is_slot_device=1
 ramdisk_compression=gzip
 " > kernelzip/props
-	cp -rp ~/android/anykernel/* kernelzip/
+	cp -rp ../anykernel3/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx9 Moob-kernel-$VERSION-tmp.zip *
+	7z a -mx0 Moob-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 Moob-kernel-$VERSION-tmp.zip ../Moob-kernel-$VERSION.zip
+	rm Moob-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al Moob-kernel-$VERSION.zip
 fi
+
+END=$(date +"%s")
+DIFF=$((END - START))
+echo -e "Kernel compiled successfully in $((DIFF / 60)) minute(s) $((DIFF % 60)) second(s)"
